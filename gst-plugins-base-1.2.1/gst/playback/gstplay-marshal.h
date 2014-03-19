d02b4f5c96fd4974da814eebcadfef0b75bc584f
Alex Christensen <achristensen07@gmail.com>
2014-03-18 18:45:36 -0600

    progress towards compiling gst-plugins-base

---
+++ gst-plugins-base-1.2.1/gst/playback/gstplay-marshal.h
@@ -0,0 +1,13 @@
+#include "gst/gstconfig.h" 
+
+#ifndef __gst_play_marshal_MARSHAL_H__
+#define __gst_play_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+G_END_DECLS
+
+#endif /* __gst_play_marshal_MARSHAL_H__ */
+
